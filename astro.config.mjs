1ecdcf86d180f453acbe5d9f0d0ffc83af85f67e
akshayrangasai <akshayrangasai.d@gmail.com>
2026-06-22 13:37:09 -0400

    Update astro.config.mjs - added starlight import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 
-const googleAnalyticsId = 'G-Z87EQFPYQ3'
+const googleAnalyticsId = 'G-Z87EQFPYQ3';
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
